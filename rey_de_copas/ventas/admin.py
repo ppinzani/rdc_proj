2c717ee596327cfa59caeafb4fd746ab8f8580b6
Paulo Pinzani <ppinzani89@gmail.com>
2018-11-19 20:42:45 -0300

    Removed extra stuff from admin page

--- rey_de_copas/ventas/admin.py
+++ rey_de_copas/ventas/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Venta, DetalleDeVenta
+#from .models import Venta, DetalleDeVenta
 
 
 # Register your models here.
-admin.site.register(Venta)
-admin.site.register(DetalleDeVenta)
+#admin.site.register(Venta)
+#admin.site.register(DetalleDeVenta)
